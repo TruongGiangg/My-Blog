dfc59154e4e331fa31c88ec39671215386e39979
TruongGiang <89589515+TruongGiangg@users.noreply.github.com>
2023-11-07 09:33:31 +0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">
-          Hoàng
+          Giang
         </a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
